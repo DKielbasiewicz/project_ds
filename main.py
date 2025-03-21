b1c3ad2cd8f6dbad8cc4c1530a62870b0660d0aa
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-03-21 21:57:29 +0100

    Add UniTrackApp and UniTrack class (skeleton of the main.py)

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-class Person:
-  def __init__(self, name):
-    self.name = name
+"""
+General structure of the UniTrack application
+"""
+
+class UniTrack:
+  def __init__(self):
+    pass
+
+class UniTrackApp:
+  def __init__(self):
+    self.__uniTrack = UniTrack()
+
+  def help(self):
+    print("List of commands:")
+    print("[1] - Add course")
+
+  def add_course(self, name, ec):
+    name = input("Enter name of the course: ")
+    ec = input("Number of credits: ")
+    pass
+
+  def run(self):
+    while True:
+      self.help()
+      print("\n")
+      command = input("Enter command: ")
+      if command == "1":
+        self.help()
+      else:
+        print("Invalid command")
