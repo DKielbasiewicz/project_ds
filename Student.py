b51b4cc51f9a089b73f6f91c2bebe6baa90e5a44
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-03-23 00:28:52 +0100

    Add str repr to Student

--- Student.py
+++ Student.py
@@ -1,8 +1,8 @@
 from Person import Person
 
 class Student(Person):
-  def __init__(self, name, surname, age, student_id):
-    super().__init__(name, surname, age)
+  def __init__(self, name, surname, age, gender,  student_id):
+    super().__init__(name, surname, age, gender)
     """I created ec, year, filed_courses and passed_courses as private
     variables because they should not be accessed directly and we
     should keept only within the class"""
@@ -12,6 +12,9 @@ class Student(Person):
     self.current_courses = []
     self._failed_courses = []
     self._passed_courses = []
+
+  def __str__(self):
+    return f"{self.name} {self.surname} is at {self._year} year and has {self._ec} credits"
 
   @property
   #The property works as an attribute, however it is only read-only
