4d8a3e983e17ea5e65234e90a49c5a622b4cb390
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-03-23 00:08:02 +0100

    Add gender attribute to Person

--- Person.py
+++ Person.py
@@ -1,8 +1,9 @@
 class Person:
-  def __init__(self, name, surname, age):
+  def __init__(self, name: str, surname: str, age: int, gender: str):
     self.__name = name
     self.__surname = surname
     self.__age = age
+    self._gender = gender
 
   @property
   def name(self):
@@ -16,5 +17,9 @@ class Person:
   def age(self):
     return self.__age
   
+  @property
+  def gender(self):
+    return self._gender
+  
   def __str__(self):
-    return f"{self.__name} {self.__surname} is {self.__age} years old"
+    return f"{self.__name} {self.__surname} is {self.__gender} and is {self.__age} years old"
