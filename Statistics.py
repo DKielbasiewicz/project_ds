c00a2c7d829b8f0aea07eda2f305f761ca618ec7
Stefan <stefan.cieslewicz03@wp.pl>
2025-03-24 18:34:36 +0100

    Created UniversityStatistics Class, and minor adjustments
    - UniversityStatistics created,
    >This class is the alternative to Statistics file and class
    >Matplotlib initial implementation
    
    -Minor adjustments in Course.py:
    >name getter and id getter added

---
+++ Statistics.py
@@ -0,0 +1,7 @@
+"""
+
+"""
+from main import University
+
+class Statistics:
+    ...
--- main.py
+++ main.py
@@ -6,36 +6,69 @@ from Course import Course
 from Student import Student 
 from Professor import Professor 
 
-class University:
-    def __init__(self):
-        self.name = "UniTrack University"
-        self.__available_courses = []
-        self.__employees = []
+import matplotlib.pyplot as plt
+#from matplotlib.ticker import MultipleLocator, FuncFormatter
+class UniversityStatistics:
+  def avg_grade_course(self, chosen_course: Course, plot_on = True):
+    sum_,min_,max_ = 0,10,0
+    final_grades = chosen_course.get_grades
+    for final_grade in final_grades:
+        grade_temp = final_grade.grade # made a temp var to avoid calling .grade over and over again
+        # sum and maxima
+        sum_ += grade_temp
+        if grade_temp > max_: max_ = grade_temp
+        elif grade_temp < min_: min_ = grade_temp
+    #average of all grades
+    avg_ = sum_/len(final_grades)
+    # if plot then do plot
+    if plot_on:
+      plt.bar([f'Lowest Grade: {min_}', f'Average Grade: {avg_}', f'Highest Grade: {max_}'],[min_, avg_, max_])
+      plt.ylabel('Grade')
+      plt.title(chosen_course)
+      plt.show()
+    # return average
+    return avg_
         
-    def avg_grade_course(self, chosen_course: Course):
-        sum_ = 0
-        final_grades = chosen_course.get_grades
-        for final_grade in final_grades:
-            sum_ += final_grade.grade
-        return sum_/len(final_grades)
-        
-    def avg_grade_uni(self):
-        all_courses = self.available_courses
-        sum_ = 0
-        for course in all_courses:
-            sum_ += self.avg_grade_course(course)
-            
-        return sum_/len(all_courses)
-        
-    def __eq__(self, another_uni) -> bool:
-        if self.avg_grade_uni() == another_uni.avg_grade_uni():
-            return True
-        return False
+  def avg_grade_uni(self):
+    all_courses = self.available_courses
+    sum_ = [0] # the first number in the list of averages is the sum of all averages, look at the second line of for loop
+    for course in all_courses:
+        sum_.append(self.avg_grade_course(course, False)) 
+        sum_[0] += sum_[-1] # This is the sum of all averages
+    #ploten machen
+    plt.plot([course.course_id for course in all_courses], sum_[1::], 'ro') 
+    plt.ylabel('Average Grade')
+    plt.title("Graph of all courses' grades")
+    plt.show()
     
-    #def __lt__(self, another_uni)
+    return sum_[0]/len(sum_[1::])
+
+class University(UniversityStatistics):
+  def __init__(self):
+    self.name = "UniTrack University"
+    self.__available_courses = []
+    self.__employees = []  
+      
+  @property
+  def available_courses(self):
+    return self.__available_courses
+  
+  def add_course(self, new_course: Course):
+    if not new_course in self.__available_courses:
+      self.__available_courses.append(new_course)
+      return
+    raise ValueError(" How to handle adding the same course? ") # <--- !!!
     
-    def __str__(self):
-        return f""
+
+  # NOTE Thou shalt decide the fate of all Comparison Operators
+  #def __eq__(self, another_uni) -> bool:
+  #    if self.avg_grade_uni() == another_uni.avg_grade_uni():
+  #        return True
+  #    return False
+  #def __lt__(self, another_uni)
+  
+  def __str__(self):
+      return f""
 
 class UniTrackApp:
   def __init__(self):
