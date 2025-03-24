0b82fef94aed7298a53fb612fa232e32401acc30
Stefan <stefan.cieslewicz03@wp.pl>
2025-03-24 22:34:56 +0100

    Applied changes

--- Statistics.py
+++ Statistics.py
@@ -1,7 +1,46 @@
-"""
+import matplotlib.pyplot as plt
+#from matplotlib.ticker import MultipleLocator, FuncFormatter
 
-"""
-from main import University
+class UniversityStatistics:
+    def avg_grade_course(self, chosen_course, plot_on = True): 
+        """
+        Args:
+            chosen_course (Course): Course obj
+            plot_on (bool, optional): This boolean decides whether to plot the course or no | this method is used in avg_grade_uni function, we don't want to see the plot for each course there. <- Defaults to True
 
-class Statistics:
-    ...
+        Returns:
+            average grade: float
+        """
+        sum_,min_,max_ = 0,10,0
+        final_grades = chosen_course.get_grades
+
+        for final_grade in final_grades:
+            grade_temp = final_grade.grade # made a temp var to avoid calling .grade over and over again
+            # sum and maxima
+            sum_ += grade_temp
+            if grade_temp > max_: max_ = grade_temp
+            elif grade_temp < min_: min_ = grade_temp
+        #average of all grades
+        avg_ = sum_/len(final_grades)
+        # if plot then do plot
+        if plot_on:
+            plt.bar([f'Lowest Grade: {min_}', f'Average Grade: {avg_}', f'Highest Grade: {max_}'],[min_, avg_, max_])
+            plt.ylabel('Grade')
+            plt.title(chosen_course)
+            plt.show()
+        # return average
+        return avg_
+        
+    def avg_grade_uni(self): # Shows the overall statistics of all the courses
+        all_courses = self.available_courses # list of all the courses
+        sum_ = [0] # the first number in the list of averages is the sum of all averages, look at the second line of for loop
+        for course in all_courses:
+            sum_.append(self.avg_grade_course(course, False)) 
+            sum_[0] += sum_[-1] # This is the sum of all averages
+        #ploten machen
+        plt.plot([course.course_id for course in all_courses], sum_[1::], 'ro')
+        plt.ylabel('Average Grade')
+        plt.title("Graph of all courses' grades")
+        plt.show()
+    
+        return sum_[0]/len(sum_[1::]) # returns the average grade of the all averages
--- main.py
+++ main.py
@@ -5,69 +5,8 @@ from Grade import Grade
 from Course import Course
 from Student import Student 
 from Professor import Professor 
+from Statistics import UniversityStatistics
 
-import matplotlib.pyplot as plt
-#from matplotlib.ticker import MultipleLocator, FuncFormatter
-class UniversityStatistics:
-  """
-  NOTE I've decided to put this class here, as putting it into separate file created some unwanted issues.
-  Although I'm not 100% sure, I believe that keeping this function next to the University Class is the best choice. If you have some thoughts, please share them   <--- TODO DELETE ME
-  
-  Class made to separate the statistical methods and plots from the main, University Class
-  
-  All available methods: (NOTE I plan on adding more)
-    avg_grade_course()
-          >calculates the avg grade for a specific course
-          >finds the min and max grade in the course
-          >creates a plot for the grades
-          
-    avg_grade_uni()
-          >calculates the avg grade for all courses
-          >creates a plot for the data 
-  """
-  
-  def avg_grade_course(self, chosen_course: Course, plot_on = True): 
-    """
-    Args:
-        chosen_course (Course): Course obj
-        plot_on (bool, optional): This boolean decides whether to plot the course or no | this method is used in avg_grade_uni function, we don't want to see the plot for each course there. <- Defaults to True
-
-    Returns:
-        average grade: float
-    """
-    sum_,min_,max_ = 0,10,0
-    final_grades = chosen_course.get_grades
-    
-    for final_grade in final_grades:
-        grade_temp = final_grade.grade # made a temp var to avoid calling .grade over and over again
-        # sum and maxima
-        sum_ += grade_temp
-        if grade_temp > max_: max_ = grade_temp
-        elif grade_temp < min_: min_ = grade_temp
-    #average of all grades
-    avg_ = sum_/len(final_grades)
-    # if plot then do plot
-    if plot_on:
-      plt.bar([f'Lowest Grade: {min_}', f'Average Grade: {avg_}', f'Highest Grade: {max_}'],[min_, avg_, max_])
-      plt.ylabel('Grade')
-      plt.title(chosen_course)
-      plt.show()
-    # return average
-    return avg_
-        
-  def avg_grade_uni(self): # Shows the overall statistics of all the courses
-    all_courses = self.available_courses # list of all the courses
-    sum_ = [0] # the first number in the list of averages is the sum of all averages, look at the second line of for loop
-    for course in all_courses:
-        sum_.append(self.avg_grade_course(course, False)) 
-        sum_[0] += sum_[-1] # This is the sum of all averages
-    #ploten machen
-    plt.plot([course.course_id for course in all_courses], sum_[1::], 'ro')
-    plt.ylabel('Average Grade')
-    plt.title("Graph of all courses' grades")
-    plt.show()
-    
-    return sum_[0]/len(sum_[1::]) # returns the average grade of the all averages
 class University(UniversityStatistics):
   def __init__(self):
     self.__name = "UniTrack University"
