47bd407acbe8e1ffdfad44f6abb86be00db67dac
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-03-24 16:35:22 +0100

    Add private class attribute encoding_style

--- Database.py
+++ Database.py
@@ -3,17 +3,18 @@ from Student import Student
 class Database:
   __students_file = "students.csv"
   __courses_file = "courses.csv"
+  __encoding_style = "utf-8"
 
   @classmethod
   def save_student(cls, new_student: Student):
-    with open(cls.__students_file, mode='a', newline='') as file:
+    with open(cls.__students_file, mode='a', newline='', encoding=cls.__encoding_style) as file:
       writer = csv.writer(file)
       writer.writerow([new_student.name, new_student.surname, new_student.age, new_student.gender, new_student.id])
 
   @classmethod
   def load_students(cls):
     students = []
-    with open(cls.__students_file, mode='r') as file:
+    with open(cls.__students_file, mode='r', encoding=cls.__encoding_style) as file:
       reader = csv.reader(file)
       for row in reader:
         student = Student(row[0], row[1], row[2], row[3], row[4])
