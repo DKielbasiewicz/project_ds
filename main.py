585d078e70661ee52a488b0a22d8a637ec8dfc2b
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-03-24 22:11:05 +0100

    Make minor changes to code and interface
    The code had too deep depth. I have refactored the code to make it easier to read.

--- main.py
+++ main.py
@@ -8,41 +8,41 @@ from Professor import Professor
 from Database import Database
 
 class University:
-    def __init__(self):
-        self.name = "UniTrack University"
-        self.__available_courses = []
-        self.__employees = []
-        self.__students = Database.load_students()
-    
-    def add_student(self, student_name: str, student_surname: str, student_age: int, student_gender: str):
-        new_student = Student(student_name, student_surname, student_age, student_gender)
-        self.__students.append(new_student)
-        Database.save_student(new_student)
-       
-    def avg_grade_course(self, chosen_course: Course):
-        sum_ = 0
-        final_grades = chosen_course.get_grades
-        for final_grade in final_grades:
-            sum_ += final_grade.grade
-        return sum_/len(final_grades)
+  def __init__(self):
+    self.name = "UniTrack University"
+    self.__available_courses = []
+    self.__employees = []
+    self.__students = Database.load_students()
+  
+  def add_student(self, student_name: str, student_surname: str, student_age: int, student_gender: str,):
+    new_student = Student(student_name, student_surname, student_age, student_gender)
+    self.__students.append(new_student)
+    Database.save_student(new_student)
+      
+  def avg_grade_course(self, chosen_course: Course):
+    sum_ = 0
+    final_grades = chosen_course.get_grades
+    for final_grade in final_grades:
+        sum_ += final_grade.grade
+    return sum_/len(final_grades)
+      
+  def avg_grade_uni(self):
+    all_courses = self.available_courses
+    sum_ = 0
+    for course in all_courses:
+        sum_ += self.avg_grade_course(course)
         
-    def avg_grade_uni(self):
-        all_courses = self.available_courses
-        sum_ = 0
-        for course in all_courses:
-            sum_ += self.avg_grade_course(course)
-            
-        return sum_/len(all_courses)
-        
-    def __eq__(self, another_uni) -> bool:
-        if self.avg_grade_uni() == another_uni.avg_grade_uni():
-            return True
-        return False
+    return sum_/len(all_courses)
+      
+  def __eq__(self, another_uni) -> bool:
+    if self.avg_grade_uni() == another_uni.avg_grade_uni():
+        return True
+    return False
     
-    #def __lt__(self, another_uni)
-    
-    def __str__(self):
-        return f""
+  #def __lt__(self, another_uni)
+  
+  def __str__(self):
+    return f""
 
 class UniTrackApp:
   def __init__(self):
@@ -77,10 +77,10 @@ class UniTrackApp:
       self.help()
       command = input("Enter command: ")
       if command == "1":
-        self.add_student()
+        print(self.add_student())
       if command == "9":
-         print("Thank you for using UniTrack!")
-         break
+        print("Thank you for using UniTrack!")
+        break
       else:
         print("Invalid command")
 
