b36c953444d99c9109fda483d15dc68d0061d593
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-03-24 22:07:00 +0100

    Fix bug with random stundet number generation
    I have fixed a bug with the random student number generation.
    The bug was caused by the fact that the random number wasn't taking into account
    that the student number is going to be loaded from a file.

--- Student.py
+++ Student.py
@@ -2,14 +2,14 @@ from Person import Person
 import random as rnd
 
 class Student(Person):
-  def __init__(self, name, surname, age, gender):
+  def __init__(self, name, surname, age, gender, given_student_id: int = None):
     super().__init__(name, surname, age, gender)
     """I created ec, year, filed_courses and passed_courses as private
     variables because they should not be accessed directly and we
     should keept only within the class"""
     self._ec = 0
     self._year = 1
-    self.__studentid = self.__random_student_id()
+    self.__studentid = self.__random_student_id(given_student_id)
     self.current_courses = []
     self._failed_courses = []
     self._passed_courses = []
@@ -17,18 +17,21 @@ class Student(Person):
   def __str__(self):
     return f"{self.name} {self.surname} is at {self._year} year and has {self._ec} credits"
   
-  def __random_student_id(self):
-    from Database import Database
-    all_students = Database.load_students()
-    while True:
-      new_id = rnd.randint(1000000, 9999999)
-      if all_students:
-        for student in all_students:
-          if student.id == new_id:
-            continue
-        return new_id
-      else:
-        return new_id
+  def __random_student_id(self, given_student_id):
+    if given_student_id == None:
+      from Database import Database
+      all_students = Database.load_students()
+      while True:
+        new_id = rnd.randint(1000000, 9999999)
+        if all_students:
+          for student in all_students:
+            if student.id == new_id:
+              continue
+          return new_id
+        else:
+          return new_id
+    else:
+      return given_student_id
 
   @property
   #The property works as an attribute, however it is only read-only
