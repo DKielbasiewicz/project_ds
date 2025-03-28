b45dff6c9599cce61bb77d050ea4d1d59dd40346
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-03-28 16:25:12 +0100

    Add random worker id and role to GeneralWorker.py

--- GeneralWorker.py
+++ GeneralWorker.py
@@ -1,11 +1,12 @@
 from Person import Person
 
 class GeneralWorker(Person):
-    def __init__(self, name: str, surname: str, age: int, gender: str, job: str, salary: int):
+    def __init__(self, name: str, surname: str, age: int, gender: str, job: str, salary: int, given_worker_id: int = None):
         super().__init__(name, surname, age, gender)
         self.__job = job
         self.__salary = salary
-
+        self.__role = "General Worker"
+        self.__worker_id = self.__random_worker_id()
     def __str__(self):
         return f"{self.name} {self.surname} is a {self.__job} and has {self.__salary} salary"
 
@@ -18,6 +19,31 @@ class GeneralWorker(Person):
         if gender.lower() == "n" or gender.lower() == "none" or gender.lower() == "other":
             return "none"
         raise NameError("Invalid gender: The Person has to be either Male (M) or Female (F) or other (N)")
+    
+    def __random_worker_id(self, given_worker_id):
+        #it generates random worker number within the given range
+        if given_worker_id == None:
+            from random import randint
+            #if it is a new worker added by app, then the default worker id is None, but
+            #if different then it is already in the database, that is why we pass the saved worker number
+            from Database import Database
+            #loading all employees
+            all_employees = Database.load_employees()
+            while True:
+                #generating the number
+                new_generated_id = randint(1000, 9999)
+                if all_employees:
+                    for employee in all_employees:
+                        # if someone has generated number then it redo the loop
+                        if employee.id == new_generated_id:
+                            continue
+                        #if nobody has generated number then it returns it as new worker id
+                    return new_generated_id
+                else:
+                    return new_generated_id
+        else:
+            #if worker is already in database then pass his worker number
+            return given_worker_id
 
     @property
     def job(self):
@@ -34,6 +60,14 @@ class GeneralWorker(Person):
     @salary.setter
     def salary(self, salary):
         self.__salary = salary
+
+    @property
+    def role(self):
+        return self.__role
+    
+    @property
+    def id(self):
+        return self.__worker_id
 
     # Overloading
     def __eq__(self, value):
